574104b20320b51843d8338a7555c5ee2aea8f7c
domitille <domi.roupy@gmail.com>
2024-03-28 22:00:10 +0100

    Ajout commandes ds fichier shell

---
+++ spring-boot-project2/script.sh
@@ -0,0 +1,21 @@
+
+# Start Backend
+echo "Starting Backend..."
+cd spring-boot-project2/spring-boot-project2/
+./mvnw spring-boot:run &
+
+# Attendre un court instant pour laisser le backend démarrer
+sleep 10
+
+# Test Backend
+echo "Testing Backend..."
+curl http://localhost:8080/demo/all
+
+# Arrêter le Backend (vous pouvez remplacer cela par une autre commande appropriée)
+echo "Stopping Backend..."
+# Vous pouvez remplacer cette ligne par une commande pour terminer le processus de Maven
+
+# Start Frontend
+echo "Starting Frontend..."
+cd frontend
+npm run serve
